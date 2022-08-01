85ec932d9c9d27631508b524b16eb26c5d0f3409
Ksenia Tereshina <92540972+ksu-drag@users.noreply.github.com>
2022-08-01 11:48:55 +0300

    React-course

---
+++ src/NewExpenses/NewExpense.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './NewExpense';
+const NewExpenses = () =>{
+    return(
+        <div className="new-expense">
+           <form></form>
+        </div>
+    )
+}
+
+export default NewExpenses;
+
---
+++ src/NewExpenses/NewForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './NewForm.css';
+
+const ExpenseForm = ()=> {
+    return(
+        <form></form>
+    )
+
+}
+
+export default ExpenseForm;

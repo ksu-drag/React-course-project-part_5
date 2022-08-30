b62ac92b4066059c2b4ec1be06574fffd72417c4
Ksenia Tereshina <92540972+ksu-drag@users.noreply.github.com>
2022-08-30 12:37:25 +0300

    React-course

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,24 +13,51 @@ const Expenses = (props) => {
     setFilterYear(e);
     console.log(e);
   };
-
+   
+  const correctYear = props.items.filter((expenses) =>{
+    return( expenses.date.getFullYear().toString() === filterYear)
+  })
+  // ---------- THIRD SOLUTION 
+  let currentNumeberExpenses = <p className='no-expenses'>No exepenses found...</p>
+  if (correctYear.length > 0) {
+    currentNumeberExpenses = correctYear.map((expenses) => (
+      <ExpenseItem 
+        key ={expenses.id}// for avoid a problem with react authentification list of data 
+        title ={expenses.title}
+        amount = {expenses.amount}
+        date = {expenses.date}
+        />
+     ));
+  }
+  
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter onRequerExpensesFilter ={expensesFilterHandler} value ={filterYear}/>
-       {props.items.map((expenses) => (
-        <ExpenseItem 
-          key ={expenses.id}// for avoid a problem with react authentification list of data 
-          title ={expenses.title}
-          amount = {expenses.amount}
-          date = {expenses.date}
-          />
-       ))}
-       
-        
+        {currentNumeberExpenses}
       </Card>
     </div>
   );
 }
 
 export default Expenses;
+
+//---------- FIRST SOLUTION
+// {correctYear.length === 0 && <div> No expenses found.</div>} 
+// {correctYear.length > 0 && correctYear.map((expenses) => (
+//     <ExpenseItem 
+//     key ={expenses.id}
+//     title ={expenses.title}
+//     amount = {expenses.amount}
+//     date = {expenses.date}/>
+//  ));}
+
+//---------- SECOND SOLUTION 
+// {correctYear.length === 0 ? <p>No expenses found.<p/> : correctYear.map((expenses) => (
+//     <ExpenseItem 
+//     key ={expenses.id}
+//     title ={expenses.title}
+//     amount = {expenses.amount}
+//     date = {expenses.date}/>
+//  )); }
+
